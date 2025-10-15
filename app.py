865be438a872f4d435d81b4c49f5c0b9b0ca77c5
mayrajimenezna <a385869@uach.mx>
2025-10-15 08:58:46 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,6 @@ radio = st.slider ("Selecciona el radio", 0.0, 10.0, 5.0)
 area = math.pi * radio**2
 # Mostrar resultado
 st.write (f"El área del círculo con radio {radio} es: {area: .2f}")
+# Calculo del perimetro
+perimetro = math.pi *2 * radio
+st.write (f"El perímetro del círculo es: {perimetro}")
