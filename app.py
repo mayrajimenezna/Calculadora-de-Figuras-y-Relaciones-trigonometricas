059d690ecc19b735a59e7b37009bc03032dd1b04
mayrajimenezna <a385869@uach.mx>
2025-10-15 09:09:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import math
 st.balloons()
 st.snow()
 st.title("Calculadora de Figuras y Relaciones Trigonometricas")
-st.image("https://www.google.com/search?sca_esv=df783cd413716f53&rlz=1C1SLLM_enMX1173MX1173&q=figura+triangulo&uds=AOm0WdE2fekQnsyfYEw8JPYozOKzrviPc2SkCy_EbmLwj3UWfI9lr2Dlj2AFsns6Hlc7uwoepRUWi7ulyv-wiUMKW8UUfsmffB8cyfZcjxte-YbXmu5VA66Q2VO56MbGkzlk__5qMhzDabe2o9lsHyCDtiHvK8TAEg&udm=2&sa=X&ved=2ahUKEwisz86CvKaQAxVuNEQIHcAmGIUQxKsJKAF6BAgUEAE&ictx=0&biw=1920&bih=953&dpr=1#vhid=Avr4a2nQbbf_9M&vssid=mosaic")
+st.image("https://www.google.com/search?q=figura+circulo&sca_esv=df783cd413716f53&rlz=1C1SLLM_enMX1173MX1173&udm=2&biw=1920&bih=953&ei=OrjvaJxiu9WQ8g--9KPJBA&ved=0ahUKEwic58ymvaaQAxW7KkQIHT76KEkQ4dUDCBE&uact=5&oq=figura+circulo&gs_lp=Egtnd3Mtd2l6LWltZyIOZmlndXJhIGNpcmN1bG8yBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBBAAGB4yBBAAGB4yBBAAGB4yBBAAGB5IiRlQ6QlYsRZwAngAkAEAmAGgAaAB6QiqAQMwLji4AQPIAQD4AQGYAgmgArcIwgIKEAAYgAQYQxiKBcICBhAAGAcYHpgDAIgGAZIHAzIuN6AHvyqyBwMwLje4B6gIwgcFMi01LjTIB0w&sclient=gws-wiz-img#vhid=EZWe-cszj17KAM&vssid=mosaic")
 
 
 # Widget para ingresar el radio
@@ -15,3 +15,6 @@ st.write (f"El área del círculo con radio {radio} es: {area: .2f}")
 # Calculo del perimetro
 perimetro = math.pi *2 * radio
 st.write (f"El perímetro del círculo es: {perimetro}")
+
+
+# Widget para ingresar la base
