db70e141a9120b9f57c5850b65cb7c1081c89c24
mayrajimenezna <a385869@uach.mx>
2025-10-15 08:55:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import math
 st.balloons()
 st.snow()
-st.title("Calculadora de Figuras y R")
+st.title("Calculadora de Figuras y Relaciones Trigonometricas")
 # Widget para ingresar el radio
 radio = st.slider ("Selecciona el radio", 0.0, 10.0, 5.0)
 # Calculo del área
